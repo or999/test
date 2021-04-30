c83be073f0213d70d2617bbb9976b43c9ce96649
or999 <13598282197@163.com>
2021-04-30 18:27:39 +0800

    test commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { PagesComponent } from './pages/pages.component';
+
+const routes: Routes = [
+  {
+    path: 'pages',
+    component:PagesComponent
+  },
+  {
+    path: '**',
+    redirectTo: 'pages',
+    pathMatch:'full'
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/pages/pages.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pages',
+  templateUrl: './pages.component.html',
+  styleUrls: ['./pages.component.css']
+})
+export class PagesComponent implements OnInit {
+
+  constructor() { }
+  mode = false;
+  dark = false;
+  ngOnInit(): void {
+  }
+  openMap: { [name: string]: boolean } = {
+    sub1: true,
+    sub2: false,
+    sub3: false
+  };
+
+  openHandler(value: string): void {
+    for (const key in this.openMap) {
+      if (key !== value) {
+        this.openMap[key] = false;
+      }
+    }
+  }
+}
